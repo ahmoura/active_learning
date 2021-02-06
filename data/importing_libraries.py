c822ca82dd04479841efef5f295f2093df632ebd
ahmoura <ahmoura1995@gmail.com>
2021-02-06 19:39:43 -0300

    :recycle: Refactoring code structure

---
+++ data/importing_libraries.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+np.seterr(divide='ignore', invalid='ignore')
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from copy import deepcopy
+
+from sklearn.model_selection import ShuffleSplit, train_test_split
+from sklearn import preprocessing
+
+from modAL.models import ActiveLearner
+from modAL.uncertainty import uncertainty_sampling
+
+from timeit import default_timer as timer
---
+++ data/set_environment.py
@@ -0,0 +1,13 @@
+TRAIN = 0
+TEST = 1
+
+cost = 10
+n_splits = 5 # number of bags cv
+total_performance_history = []
+k = 3 #parameter to knn classifier
+
+strategies = {"Uncertain Sampling": 0, "Random Sampling": 1, "Query by Committee": 2, "Expected Error Reduction": 3, "Expected Error Reduction": 4}
+
+#strategies = { 0: "Uncertain Sampling", 1: "Random Sampling", 2: "Query by Committee", 3:"Expected Error Reduction", 4: "Expected Error Reduction"}
+
+# list(strategies.keys())[list(strategies.values())[0]]
