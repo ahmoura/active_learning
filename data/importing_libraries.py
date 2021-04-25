1597630a957a69155188a662e6d08dc029e54bad
Antônio Rodrigues <ahmoura1995@gmail.com>
2021-04-25 17:47:25 -0300

    :recycle: ajustando arquivos .py e criando arquivo config para pyahrd

--- data/importing_libraries.py
+++ data/importing_libraries.py
@@ -4,13 +4,18 @@ np.seterr(divide='ignore', invalid='ignore')
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from copy import deepcopy
 
-from sklearn.model_selection import ShuffleSplit, train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit, ShuffleSplit, train_test_split
 from sklearn import preprocessing
 
 from modAL.models import ActiveLearner
 from modAL.uncertainty import uncertainty_sampling
 
 from timeit import default_timer as timer
+
+from scipy.io import arff
+
+from tqdm.tqdm import tqdm, trange
