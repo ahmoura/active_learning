57258cd4f9efb489eed7107dc4ad94ba4573c40c
Antônio Rodrigues <ahmoura1995@gmail.com>
2021-10-07 13:35:38 -0300

    atualizando e gerando novas visualizacoes

--- ALIH/main.py
+++ ALIH/main.py
@@ -8,11 +8,11 @@ from environment.config import *
 if __name__ == '__main__':
 
     #baseline_thread = Thread(target=run_baseline, args=(datasets,))
-    modal_thread = Thread(target=run_modal, args=(datasets,))
+    #modal_thread = Thread(target=run_modal, args=(datasets,))
     pyhard_thread = Thread(target=run_pyhard, args=(datasets,))
-    pyhard_unlabeled_thread = Thread(target=run_unlabeled_pyhard, args=(datasets,))
+    #pyhard_unlabeled_thread = Thread(target=run_unlabeled_pyhard, args=(datasets,))
 
     #baseline_thread.start()
-    modal_thread.start()
+    #modal_thread.start()
     pyhard_thread.start()
-    pyhard_unlabeled_thread.start()
+   # pyhard_unlabeled_thread.start()
